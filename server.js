b74b2988b78030d4a5d9faf7219c162759a08252
hanishkumar606 <hanishkumar606@gmail.com>
2022-11-09 12:31:18 +0530

    changed server port

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ app.get('/', (req, res) => res.send('Hello Hanish!!! I am backghhh'));
 
 
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port:${port}`));
